271fd58c7aed47bafda6bca549f87d85ccf95615
franklin <franklin.arias89@gmail.com>
2021-07-19 17:30:07 -0500

    Se hizo un ajuste para incluir un archivo .ini que contendra la configuración inicial y en caso de no existir que cree el archivo

--- ejecutar.py
+++ ejecutar.py
@@ -9,6 +9,9 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 from graficarfuturo import graficar_futuro
+import configparser
+import ast
+from configupdater import ConfigUpdater
 
 path_img = "img"
 path_report = "reportes"
@@ -28,19 +31,79 @@ if os.path.exists("tmp/archivo.csv"):
 if not os.path.isdir("tmp"):
     os.mkdir("tmp")
 
+configuracion = configparser.ConfigParser(allow_no_value=True, comment_prefixes='#')
+
+
+def comprobar_config():
+    if os.path.isfile('configuracion.ini'):
+        configuracion.read('configuracion.ini')
+    else:
+        print("no se encontro")
+        updater = ConfigUpdater()
+        updater.add_section('General')
+
+        updater.set('General', 'symbols', '["EURUSD=X", "USDJPY=X"]')
+        updater['General']['symbols'].add_before.comment('# Se define los simbolos a procesar, Mercados/Divisas/CFDs',
+                                                         comment_prefix='#')
+        updater.set('General', '# symbols', '["AUDCAD=X","AUDCHF=X","AUDNZD=X","AUDUSD=X","CHFJPY=X","EURCAD=X",'
+                                            '"EURUSD=X","EURGBP=X","EURJPY=X","EURCHF=X","USDCAD=X","USDCHF=X",'
+                                            '"USDJPY=X","GBPAUD=X","GBPUSD=X","GBPJPY=X","NZDCAD=X","NZDUSD=X",'
+                                            '"NZDJPY=X"]')
+        updater.set('General', 'ciclos', '4')
+        updater['General']['ciclos'].add_before.comment(
+            '# Es la cantidad de dias a graficar para la tendencia',
+            comment_prefix='#')
+        updater.add_section('Tensorflow')
+        updater.set('Tensorflow', 'N_STEPS', '7')
+        updater['Tensorflow']['N_STEPS'].add_before.comment('# En este apartado se configuran parametros Tensorflow',
+                                                            comment_prefix='#')
+        updater['Tensorflow']['N_STEPS'].add_before.comment('# "N_STEPS" Es la secuencia en dias para encontrar'
+                                                            ' el patron, por default esta en 7 dias que corresponde'
+                                                            ' a una semana',
+                                                            comment_prefix='#')
+        updater.set('Tensorflow', 'date_start', '2020-01-01')
+        updater['Tensorflow']['date_start'].add_before.comment('# "date_start" Fecha inicio para el analisis en '
+                                                               'formato YYYY-MM-DD',
+                                                               comment_prefix='#')
+        updater.set('Tensorflow', 'N_LAYERS', '4')
+        updater['Tensorflow']['N_LAYERS'].add_before.comment('# "N_LAYERS" Numero de capas para el modelo',
+                                                             comment_prefix='#')
+        updater.set('Tensorflow', 'UNITS', '256')
+        updater['Tensorflow']['UNITS'].add_before.comment('# "UNITS" Numero de neuronas por default 256',
+                                                          comment_prefix='#')
+        updater.set('Tensorflow', 'LOSS', 'mae')
+        updater['Tensorflow']['LOSS'].add_before.comment('# "LOSS" Es la medida para determinar errores del pronostico,'
+                                                         ' por default "mae" que significa "error absluto medio"',
+                                                         comment_prefix='#')
+        updater.set('Tensorflow', 'OPTIMIZER', 'adam')
+        updater['Tensorflow']['OPTIMIZER'].add_before.comment('# "OPTIMIZER" Definimos el algoritmo que se usara en'
+                                                              ' nuestro modelo, por defecto se tiene definido "adam"',
+                                                              comment_prefix='#')
+        updater.set('Tensorflow', 'EPOCHS', '200')
+        updater['Tensorflow']['EPOCHS'].add_before.comment('# "EPOCHS" Aca definimos el ciclo de pruebas que debe'
+                                                           ' repetir hasta encontrar el resultado mas optimo'
+                                                           ' por default se tiene definido 200',
+                                                           comment_prefix='#')
+        with open('configuracion.ini', 'w') as f:
+            updater.write(f)
+        configuracion.read('configuracion.ini')
+
+
+comprobar_config()
+
+general = configuracion['General']
+symbols = ast.literal_eval(general['symbols'])
+ciclos = int(general['ciclos'])
+
 
 class Symbols:
-    # listado = ['AUDCAD=X', 'AUDCHF=X', 'AUDNZD=X', 'AUDUSD=X', 'CHFJPY=X', 'EURCAD=X', 'EURUSD=X', 'EURGBP=X',
-    #            'EURJPY=X', 'EURCHF=X', 'USDCAD=X', 'USDCHF=X', 'USDJPY=X', 'GBPAUD=X', 'GBPUSD=X', 'GBPJPY=X',
-    #            'NZDCAD=X', 'NZDUSD=X', 'NZDJPY=X']
-    listado = ['EURUSD=X', 'USDJPY=X']
-    # listado = ['EURUSD=X']
+    listado = symbols
 
 
 def ejecutar():
     limite = len(Symbols.listado)
 
-    veces = 4
+    veces = ciclos
     for a in range(limite):
         for i in range(veces):
             os.system(f"python train.py {Symbols.listado[a]} {int(i)}")
@@ -52,7 +115,6 @@ def leercsv():
     with open('tmp/archivo.csv', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
-    # print(data[0])
     return data
 
 
@@ -110,7 +172,7 @@ def email(tipo):
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
     session.quit()
-    print('Mail Sent')
+    print(f'Mail Sent {tipo}')
 
 
 ejecutar()
@@ -118,4 +180,4 @@ crearpdf(titulo='Backtesting', tipo='backtesting')
 crearpdf(titulo='Tendencia', tipo='tendencia')
 email(tipo='backtesting')
 email(tipo='tendencia')
-# os.system("rm -rf data logs img csv-results *.pdf tmp results")
+os.system("rm -rf data logs img csv-results *.pdf tmp results")
