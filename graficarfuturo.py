f5b53da0652f3d962def2568e76ac736b4801fe8
franklin <franklin.arias89@gmail.com>
2021-07-25 11:03:03 -0500

    Se actualizo graficarfuturo.py para que muestre 4 decimales en lugar de 3

--- graficarfuturo.py
+++ graficarfuturo.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import csv
 
+my_formatter = "{0:10.4f}"
+
 
 def graficar_futuro(ticker):
     w = []
@@ -10,14 +12,15 @@ def graficar_futuro(ticker):
     with open(f'tmp/{ticker}_futuro.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
-            y.append(float(row[3]))
-            w.append(float(row[2]))
-            z.append(float(row[1]))
+            y.append(my_formatter.format(float(row[3])))
+            w.append(my_formatter.format(float(row[2])))
+            z.append(my_formatter.format(float(row[1])))
             x.append(str(row[0]))
     plt.figure(figsize=(10, 5))
-    plt.plot(x, y, label='Precio Cierre', marker="o")
     plt.plot(z, label='Precio Bajo', marker="o")
+    plt.plot(x, y, label='Precio Cierre', marker="o")
     plt.plot(w, label='Precio Alto', marker="o")
+
     plt.xlabel('Tiempo')
     plt.ylabel('Precio')
     plt.title(f'Proyección de precio\n{ticker}')
