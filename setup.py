7070a1195b7cdf214a4e7c3d98016b09416de096
franklin <franklin.arias89@gmail.com>
2021-08-10 17:56:31 -0500

    Se agrega requerimientos

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ia-tradin',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='franklin',
+    author_email='franklin.arias89@gmail.com',
+    description=''
+)
